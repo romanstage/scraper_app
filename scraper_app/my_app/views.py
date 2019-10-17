699f85cfc3d44437e14569523ac5c09385367412
romanstage <r.parpolita@gmail.com>
2019-10-17 18:06:36 +0300

    Добавил фронтенд

--- scraper_app/my_app/views.py
+++ scraper_app/my_app/views.py
@@ -1,7 +1,16 @@
 import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 from .import models
 
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = {
+        'search':search,
+    }
+
+    return render (request, 'my_app/new_search.html',stuff_for_frontend)
